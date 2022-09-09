4d59a651cd235b4e48a086610391d09aa8318201
hackerman70000 <112731017+hackerman70000@users.noreply.github.com>
2022-09-10 00:51:30 +0200

    Update tf.py

--- tf.py
+++ tf.py
@@ -1,32 +1,27 @@
-import ta
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
+import ta
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
-from keras.callbacks import EarlyStopping
-from sklearn.metrics import precision_score
-
 
 # data downloaded from https://www.cryptodatadownload.com/data/binance/
-df = pd.read_csv('data_binance.csv')
 
+
+# data setup
+df = pd.read_csv('data_binance.csv')
 df.rename(columns = {'Volume BTC':'volume'}, inplace = True)
 df['date'] = pd.to_datetime(df['date'])
 df = df.sort_values(by="date")
 df.reset_index(drop=True, inplace=True)
 
-# deleting unused data
+
+# deleting unwanted data
 df.drop('date', inplace=True, axis=1)
-df.drop('tradecount', inplace=True, axis=1)
-df.drop('unix', inplace=True, axis=1)
-df.drop('symbol', inplace=True, axis=1)
-df.drop('Volume USDT', inplace=True, axis=1)
-
-
+df.drop(['tradecount','unix','symbol','Volume USDT'], inplace=True, axis=1)
 df.dropna(inplace=True)
+
 
 # calculating technical analisis indicators
 df['rsi_14'] = ta.momentum.RSIIndicator(close=df["close"], window=14).rsi()
@@ -47,11 +42,10 @@ df['sto'] = ta.momentum.StochasticOscillator(high=df["high"], low=df["low"], clo
 df['uo'] = ta.momentum.UltimateOscillator(high=df["high"], low=df["low"], close=df["close"], window1 = 7, window2 = 14, window3 = 28, weight1 = 4.0, weight2 = 2.0, weight3 = 1.0).ultimate_oscillator()
 df['cci'] = ta.trend.CCIIndicator(high=df["high"], low=df["low"], close=df["close"], window = 20, constant = 0.015).cci()
 df['atr'] = ta.volatility.AverageTrueRange(high=df["high"], low=df["low"], close=df["close"], window = 14).average_true_range()
-
 df.dropna(inplace=True)
 
-# creating target values
 
+# creating target values
 df['exp']  = np.where(  (df['open'] * 1.051 <= df['high'].shift(-1))
                       | (df['open'] * 1.051 <= df['high'].shift(-2))
                       | (df['open'] * 1.051 <= df['high'].shift(-3))
@@ -61,55 +55,65 @@ df['exp']  = np.where(  (df['open'] * 1.051 <= df['high'].shift(-1))
                        ,1 ,0)
 
 
-
-df.drop(df.tail(6).index,inplace=True) #do wywalenia
-df.drop('high', inplace=True, axis=1)
-df.drop('open', inplace=True, axis=1)
-df.drop('volume', inplace=True, axis=1)
-df.drop('close', inplace=True, axis=1)
-df.drop('low', inplace=True, axis=1)
+# deleting unwanted data
+df.drop(df.tail(6).index,inplace=True) #- comment if making predicitons
+df.drop(['high','open','volume','close','low'], inplace=True, axis=1)
 df.dropna(inplace=True)
 
-df = df.iloc[df.shape[0]- 720: , :]   #do testu
 
-df_train = df.iloc[:df.shape[0]- 5,:]
-df_pred =  df.iloc[df.shape[0]- 5:,:]
+# df based on last 2 years
+df = df.iloc[df.shape[0]- 720: , :]
 
-df_train.reset_index(drop=True, inplace=True)
+
+# creating prediction dataset
+df = df.iloc[:df.shape[0]- 10,:]  # declare how many last days you wanna predict
+df_pred =  df.iloc[df.shape[0]- 10:,:]
+
+
+# resetting index to troubleshoot
+df.reset_index(drop=True, inplace=True)
 df_pred.reset_index(drop=True, inplace=True)
-# splitting into dataset and validation sets
-x = df_train.iloc[:, :len(df_train.columns) - 1]
-y = df_train['exp']
 
-x_test2 = df_pred.iloc[:, :len(df_pred.columns) - 1]
-y_test2 = df_pred['exp']
+
+# splitting into dataset and validation sets
+x = df.iloc[:, :len(df.columns) - 1]
+y = df['exp']
+
+x_pred = df_pred.iloc[:, :len(df_pred.columns) - 1]
+y_pred = df_pred['exp']
+
 
 # checking if nan occurs
 print(x.isnull().any().any())
 print(y.isnull().any().any())
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.15,shuffle = True )
+
+# train-test split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.1,shuffle = True )
 
 
-# skalowanie danych
+# scaling
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaler.fit(x_train)
 
 x_train_scaled = scaler.transform(x_train)
 x_test_scaled = scaler.transform(x_test)
-x_test2_scaled = scaler.transform(x_test2)
+x_pred_scaled = scaler.transform(x_pred)
 
-# pca
-pca = PCA(n_components=18)
+
+# PCA
+pca = PCA(n_components=16)
 pca.fit(x_train_scaled)
 
 x_train_PCA = pca.transform(x_train_scaled)
 x_test_PCA = pca.transform(x_test_scaled)
-x_test2_PCA = pca.transform(x_test2_scaled)
+x_pred_PCA = pca.transform(x_pred_scaled)
 
+
+# architecture
 model = tf.keras.Sequential(
     [
-        #tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Dense(128, activation="relu"),
         tf.keras.layers.Dense(32, activation="relu"),
         tf.keras.layers.Dense(16, activation="relu"),
@@ -117,34 +121,35 @@ model = tf.keras.Sequential(
     ]
 )
 
-es = EarlyStopping(monitor='val_loss', patience=10,  mode='min', verbose=1)
-
-model.compile(
-    loss='binary_crossentropy', optimizer="Adam", metrics=[tf.keras.metrics.Precision()]
-)
-
-history = model.fit(x_train_PCA, y_train, batch_size=1, epochs=13, validation_split=0.1, shuffle=True) #,callbacks=[es]
-model.summary()
+model.compile(loss='binary_crossentropy', optimizer="Adam", metrics=[tf.keras.metrics.Precision()])
 
 
-print("Evaluate on test data")
+# training
+model.fit(x_train_PCA, y_train, batch_size=1, epochs=20,validation_split=0.1,validation_data=None,shuffle=True)
+print("")
+
+# test
 evaluation = model.evaluate(x_test_PCA , y_test, batch_size=1)
 print("test loss, test acc:", evaluation)
+print("")
+
+# prediction
+predictions = pd.DataFrame(model.predict(x_pred_PCA),columns=['0-1'])
+predictions['target'] = y_pred
+predictions['predictions']  = np.where(predictions['0-1'] > 0.92,1 ,0)
 
 
-plt.plot(history.history['precision'])
-plt.plot(history.history['val_precision'])
-plt.title('model accuracy')
-plt.ylabel('precision')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
+pd.set_option('display.max_rows', None)
+print(predictions)
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
-plt.show()
 
+# prediction precision (tp/(fp+tp)
+m = tf.keras.metrics.Precision()
+m.update_state(predictions['target'], predictions['predictions'])
+print("")
+print("precision :", end = ' ')
+print(m.result().numpy())
+
+print("")
+print("sum of 1 :", end = ' ')
+print(predictions['predictions'].sum())
